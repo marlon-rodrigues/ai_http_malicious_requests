7b2029c69cdabe524b52d5f635830e25a95e7fca
Marlon Geraldo Rodrigues Viana <marlongeraldorodriguesviana@item-ax30880.wks.axway.int>
2018-11-07 09:28:34 +0000

    Added cors support to server app

--- Server/axway_ai/app.js
+++ Server/axway_ai/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 global.appRoot = path.resolve(__dirname);
 
@@ -15,6 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
